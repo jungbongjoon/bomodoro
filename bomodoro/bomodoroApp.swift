9df428d613ab35a2f16da2f13a38e5b6b73977f6
정봉준 <devbyjune@gmail.com>
2025-08-01 18:01:43 +0900

    Initial Commit

---
+++ bomodoro/bomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  bomodoroApp.swift
+//  bomodoro
+//
+//  Created by bong on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct bomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
